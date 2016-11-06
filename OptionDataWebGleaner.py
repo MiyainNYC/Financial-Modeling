b215f5bb8b9391848d113efbf452bf946e78b62c
Miya Wang <nwang26@fordham.edu>
2016-11-05 21:33:19 -0400

    Add files via upload

--- OptionDataWebGleaner.py
+++ OptionDataWebGleaner.py
@@ -1,4 +1,3 @@
-from __future__ import division
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import os
@@ -18,38 +17,41 @@ class OptionDataWebGleaner(object):
         ticker = [line.strip() for line in open('Yahoo_ticker_List.csv')]
         self.today = date.today()
 
-        num = int(raw_input("How many stocks you want to download?\n"))
+        num = int(input("How many stocks you want to download? There are %d options in total\n"% len(ticker)))
 
         list_chosen = random.sample(range(0, len(ticker)), num)
 
         stocks = [ticker[i] for i in list_chosen]
 
-        if raw_input('Do you have a stock you want to look particularly? Type Y or yes, N or no\n') == 'Y':
-            stocks.append(raw_input('Type the ticker of your stock\n'))
+        if input('Do you have a stock you want to look particularly? Type Y or yes, N or no\n') == 'Y':
+            stocks.append(input('Type the ticker of your stock\n'))
 
         df_dict = self.data_download(stocks)
 
-        if raw_input('Do you want to save the date? Type Y or yes, N or no\n') == 'Y':
-            for stock in df_dict.keys():
-                df_dict[stock].to_csv(stock+'.csv')
-
-        if raw_input('Do you want to process and clean the data? Type Y or yes, N or no\n') == 'Y':
+        if input('Do you want to process and clean the data? Type Y or yes, N or no\n') == 'Y':
             for stock in df_dict.keys():
                 df = df_dict[stock]
                 df_dict[stock] = self.clean_data(df)
 
-        if raw_input("Do you want to remove outliers? Type Y for yes, N or no\n") == 'Y':
+
+        if input('Do you want to save the data into csv form? Type Y or yes, N or no\n') == 'Y':
+            for stock in df_dict.keys():
+                os.chdir("C:\\Users\\Miya\\OneDrive\\Miya'sGithub\\Financial-Modeling\\projectOne\\dataset")
+                df_dict[stock].to_csv(stock+'.csv')
+
+
+        if input("Do you want to remove outliers? Type Y for yes, N or no\n") == 'Y':
             for stock in df_dict.keys():
                 print(df_dict[stock].loc[df_dict[stock]['Implied Volatility'] > 2])
                 print('Removing outliers...')
                 df_dict[stock] = df_dict[stock].loc[df_dict[stock]['Implied Volatility'] <= 2]
 
-        if raw_input("Do you want to visualize? Type Y for yes, N or no\n") == 'Y':
+        if input("Do you want to visualize? Type Y for yes, N or no\n") == 'Y':
             for stock in df_dict.keys():
-                print stock
-                self.viz(df_dict[stock])
+                print(stock)
+                self.viz(df_dict[stock],stock)
 
-        if raw_input("Do you want to get summery of your stock? Type Y for yes, N or no\n") == 'Y':
+        if input("Do you want to get summery of your stock? Type Y for yes, N or no\n") == 'Y':
             for stock in df_dict.keys():
                 self.summary(df_dict[stock])
 
@@ -102,7 +104,7 @@ class OptionDataWebGleaner(object):
 
                 if soup.find('table', 'calls') is not None:
 
-                    print('Web Crawling~')
+                    print('Web Crawling.....')
 
                     stock_price = [float(i.text) for i in soup.findAll('span', 'Fz(36px)')]
                     title = [i.text for i in soup.find('table', 'calls').find_all('th')]
@@ -139,20 +141,22 @@ class OptionDataWebGleaner(object):
 
         return df_dict
 
-    def viz(self, dataframe):
+    def viz(self, dataframe,stock):
         time_to_maturity = []
         dataframe = dataframe.sort_values(by='Strike')
         ## grab dataframe, then relevant data
         for i, j in zip(dataframe.maturity_date, dataframe.date):
             time_to_maturity.append((i - j).days / 365)
 
-        print time_to_maturity
+        print(time_to_maturity)
 
         strike_price = dataframe['Strike']
 
         # generate pseudo-implied volatility by using strike price and time-to-maturity as parameters
 
         implied_vol = dataframe['Implied Volatility'].values
+
+        print(implied_vol)
 
         strike_price, time_to_maturity = np.meshgrid(strike_price, time_to_maturity)
 
@@ -169,6 +173,9 @@ class OptionDataWebGleaner(object):
         ax.set_xlabel('Strike Price')
         ax.set_ylabel('time to maturity')
         ax.set_zlabel('implied volatility%')
+        plot.suptitle(stock)
+        os.chdir("C:\\Users\\Miya\\OneDrive\\Miya'sGithub\\Financial-Modeling\\projectOne\\pic")
+        fig.savefig(stock+ '.png', dpi=fig.dpi)
         plot.show()
 
     def summary(self, dataframe):
@@ -177,3 +184,6 @@ class OptionDataWebGleaner(object):
 
 
 OptionDataWebGleaner()
+
+
+
